73579c9a424f474264d52536f6450f537ddff666
jjoo <jjs576@naver.com>
2021-06-06 17:01:25 +0900

    Add : philo_three

---
+++ philo_three/srcs/routine.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjoo <jjoo@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/03 22:05:20 by jjoo              #+#    #+#             */
+/*   Updated: 2021/06/06 16:40:15 by jjoo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static void	print_message(t_philo *philo, char *state)
+{
+	printf("%lums %d%s", get_time() - philo->start_time, philo->index, state);
+}
+
+static void	philo_eat(t_philo *p)
+{
+	sem_wait(p->forks);
+	print_message(p, MESSAGE_FORK);
+	print_message(p, MESSAGE_FORK);
+	print_message(p, MESSAGE_EAT);
+	p->last_eat = get_time();
+	ft_sleep(ft_min(p->time_to_eat, p->time_to_die));
+	p->eat_count++;
+	sem_post(p->forks);
+	p->state &= ~STATE_EAT;
+	p->state |= STATE_SLEEP;
+}
+
+static void	philo_sleep(t_philo *p)
+{
+	size_t	time1;
+	size_t	time2;
+
+	print_message(p, MESSAGE_SLEEP);
+	time1 = ft_max(p->time_to_eat, p->time_to_sleep);
+	time2 = p->time_to_die - p->time_to_eat;
+	ft_sleep(ft_min(time1, time2));
+	p->state &= ~STATE_SLEEP;
+	p->state |= STATE_THINK;
+}
+
+static void	philo_think(t_philo *p)
+{
+	print_message(p, MESSAGE_THINK);
+	p->state &= ~STATE_THINK;
+	p->state |= STATE_EAT;
+}
+
+void		routine(t_philo *p)
+{
+	p->state |= STATE_EAT;
+	while (p->state & STATE_RUN)
+	{
+		if (p->eat_count == p->num_of_must_eat)
+			p->state = STATE_OVER;
+		if (get_time() - p->last_eat >= p->time_to_die)
+			p->state = STATE_DEAD;
+		if (p->state & STATE_EAT)
+			philo_eat(p);
+		else if (p->state & STATE_SLEEP)
+			philo_sleep(p);
+		else if (p->state & STATE_THINK)
+			philo_think(p);
+	}
+	if (p->state == STATE_DEAD)
+		print_message(p, MESSAGE_DIE);
+	exit(p->state);
+}
---
+++ philo_three/srcs/run.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   run.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjoo <jjoo@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/03 22:54:01 by jjoo              #+#    #+#             */
+/*   Updated: 2021/06/06 17:00:23 by jjoo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int		get_philos_state(t_info *info)
+{
+	int	state;
+	int	i;
+
+	i = -1;
+	state = 0;
+	while (++i < info->num_of_philo)
+	{
+		waitpid(info->philos[i].pid, &info->philos[i].status, WNOHANG);
+		if (WIFEXITED(info->philos[i].status))
+			state |= WEXITSTATUS(info->philos[i].status);
+	}
+	return (state);
+}
+
+void	set_philos_kill(t_info *info)
+{
+	int		i;
+
+	i = -1;
+	while (++i < info->num_of_philo)
+		kill(info->philos[i].pid, SIGKILL);
+}
+
+void	run(t_info *info)
+{
+	int	state;
+	int	i;
+
+	while (1)
+	{
+		state =	get_philos_state(info);
+		if (state & STATE_DEAD)
+			set_philos_kill(info);
+		if (state == STATE_OVER || state == STATE_DEAD)
+			break ;
+	}
+	i = -1;
+	while (++i < info->num_of_philo)
+		waitpid(info->philos[i].pid, NULL, 0);
+}
